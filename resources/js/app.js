ea9628520d89c0bb416b4eaf1132de788dabc3a6
Irisia <pochara.su@ku.th>
2022-11-30 00:00:41 +0700

    add auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,7 @@
 import './bootstrap';
 
-import { createApp } from 'vue'
-import Hello from "./components/Hello";
-import RegisterPopup from "./components/RegisterPopup";
-import EmployeeCard from "./components/EmployeeCard";
-import CountDown from "./components/CountDown";
-import MqttTest from "./components/MqttTest";
-import VueSweetalert2 from 'vue-sweetalert2';
+import Alpine from 'alpinejs';
 
-const app = createApp({})
+window.Alpine = Alpine;
 
-app.component('hello', Hello);
-app.component('register-popup', RegisterPopup);
-app.component('employee-card', EmployeeCard)
-app.component('count-down', CountDown);
-app.component('mqtt-test', MqttTest);
-
-app.use(VueSweetalert2);
-app.mount('#app')
+Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +15,17 @@ use App\Http\Controllers\EmployeeController;
 */
 
 Route::get('/', function () {
-//    if \Illuminate\Support\Facades\Auth::user()
-    return redirect()->route('register.index');
+    return view('welcome');
 });
 
-Route::group(['prefix' => 'register'], function() {
-    Route::get('', [EmployeeController::class, 'index'])->name('register.index');
-    Route::get('search', [EmployeeController::class, 'search'])->name('register.search');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('qr-code/{qr_code}', [EmployeeController::class, 'show'])->name('qr-code.show');
-
-Route::get('lucky-draw', function () {
-    return view('lucky-draw.index');
-});
-
+require __DIR__.'/auth.php';
