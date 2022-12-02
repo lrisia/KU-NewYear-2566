88378d2007c000d13c1d2b0f072d87ce9092c31a
Saac Sornchai <sornchai.la@ku.th>
2022-12-02 13:44:34 +0700

    command to unregister register:clear

---
+++ app/Console/Commands/ClearRegister.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Employee;
+use Illuminate\Console\Command;
+
+class ClearRegister extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'register:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->ask("Which email you want to unregister?");
+        $employee = Employee::where('email', $email)->first();
+        if (is_null($employee)) {
+            $this->line("No {$email} found in registered employee");
+            return Command::SUCCESS;
+        }
+        $this->table([
+            'p_id', 'name', 'organization', 'register_at', 'qr_code'
+        ], [
+            [
+                $employee->p_id,
+                $employee->name,
+                $employee->organizer->name,
+                $employee->register_at->format('d M H:i:s'),
+                $employee->qr_code
+            ]
+        ]);
+
+        $confirmed = $this->confirm("Are you sure to unregister this employee?", false);
+        if ($confirmed) {
+            $employee->register_at = null;
+            $employee->qr_code = null;
+            $employee->email = null;
+            $employee->save();
+            $this->line("This employee has been unregistered");
+        }
+        return Command::SUCCESS;
+    }
+}
