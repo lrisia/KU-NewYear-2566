9343a531784ae35fb6f1a3b81fbe9d0b9a7eee18
Saac Sornchai <sornchai.la@ku.th>
2022-12-26 15:28:38 +0700

    entrance registration login with qr code

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -33,6 +34,15 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    public function qrLogin(Request $request, $email, $token) {
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        $user = User::where('email', $email)->where('remember_token', $token)->firstOrFail();
+        $request->session()->regenerate();
+        Auth::loginUsingId($user->id);
+        return redirect()->route('qr-code.scan');
     }
 
     /**
